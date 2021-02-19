89cc6e05c8c6a9032ae2b22888dd9d970a7355c0
bigkitttty <caleb.n.coblentz@gmail.com>
2021-02-19 11:11:20 -0800

    Update formatutils.cpp

--- gfx/angle/src/libANGLE/formatutils.cpp
+++ gfx/angle/src/libANGLE/formatutils.cpp
@@ -1,9 +1,5 @@
 
-    GLuint rows =
-        (imageHeight > 0 ? static_cast<GLuint>(imageHeight) : static_cast<GLuint>(height));
-    CheckedNumeric<GLuint> pixelsHeight(imageHeight > 0 ? static_cast<GLuint>(imageHeight)
-                                                        : static_cast<GLuint>(height));
-
+    
     CheckedNumeric<GLuint> rowCount;
     if (compressed) {
         CheckedNumeric<GLuint> checkedBlockHeight(compressedBlockHeight);
@@ -16,7 +12,6 @@
     ANGLE_TRY_RESULT(computeRowPitch(formatType, width, alignment, rowLength), rowPitch);
 
     CheckedNumeric<GLuint> checkedRowPitch(rowPitch);
-    auto depthPitch = checkedRowPitch * rows;
     auto depthPitch = checkedRowPitch * rowCount;
     ANGLE_TRY_CHECKED_MATH(depthPitch);
     return depthPitch.ValueOrDie();
