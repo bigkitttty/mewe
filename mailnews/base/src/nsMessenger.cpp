8110588faa1a53d9539c64cc105c1a50849159d4
bigkitttty <caleb.n.coblentz@gmail.com>
2021-02-26 14:50:05 -0800

    Update nsMessenger.cpp

--- mailnews/base/src/nsMessenger.cpp
+++ mailnews/base/src/nsMessenger.cpp
@@ -4,3 +4,17 @@
                        nsIFilePicker::modeSave, false);
       filePicker->SetDefaultString(path);
       filePicker->AppendFilters(nsIFilePicker::filterAll);
+  GetString(NS_LITERAL_STRING("SaveAllAttachments"), saveAttachmentStr);
+  filePicker->Init(mWindow,
+                   saveAttachmentStr,
+                   nsIFilePicker::modeGetFolder, false);
+
+  rv = GetLastSaveDirectory(getter_AddRefs(lastSaveDir));
+  if (NS_SUCCEEDED(rv) && lastSaveDir)
+         nsString chooseFolderStr;
+  GetString(NS_LITERAL_STRING("ChooseFolder"), chooseFolderStr);
+  filePicker->Init(mWindow, chooseFolderStr, nsIFilePicker::modeGetFolder, false);
+
+  nsCOMPtr<nsIFile> lastSaveDir;
+  rv = GetLastSaveDirectory(getter_AddRefs(lastSaveDir));
+
