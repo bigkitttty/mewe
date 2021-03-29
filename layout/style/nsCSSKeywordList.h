f69048340dc0f1cd2132a6e0b919ff526ab2f61c
bigkitttty <caleb.n.coblentz@gmail.com>
2021-03-29 11:33:07 -0700

    Issue #1755 - Add smooth, high-quality and pixelated to CSS image-rendering

---
+++ layout/style/nsCSSKeywordList.h
@@ -0,0 +1,30 @@
+CSS_KEY(help, help)
+CSS_KEY(hidden, hidden)
+CSS_KEY(hide, hide)
+CSS_KEY(high-quality, high_quality)
+CSS_KEY(highlight, highlight)
+CSS_KEY(highlighttext, highlighttext)
+CSS_KEY(historical-forms, historical_forms)
+CSS_KEY(perspective, perspective)
+CSS_KEY(petite-caps, petite_caps)
+CSS_KEY(physical, physical)
+CSS_KEY(pixelated, pixelated)
+CSS_KEY(plaintext, plaintext)
+CSS_KEY(pointer, pointer)
+CSS_KEY(polygon, polygon)
+
+
+
+
+const KTableEntry nsCSSProps::kImageRenderingKTable[] = {
+  { eCSSKeyword_auto, NS_STYLE_IMAGE_RENDERING_AUTO },
+  { eCSSKeyword_high_quality, NS_STYLE_IMAGE_RENDERING_OPTIMIZEQUALITY },
+  { eCSSKeyword_optimizespeed, NS_STYLE_IMAGE_RENDERING_OPTIMIZESPEED },
+  { eCSSKeyword_optimizequality, NS_STYLE_IMAGE_RENDERING_OPTIMIZEQUALITY },
+  { eCSSKeyword_pixelated, NS_STYLE_IMAGE_RENDERING_CRISPEDGES },
+  { eCSSKeyword_smooth, NS_STYLE_IMAGE_RENDERING_OPTIMIZESPEED },
+  { eCSSKeyword__moz_crisp_edges, NS_STYLE_IMAGE_RENDERING_CRISPEDGES },
+  { eCSSKeyword_UNKNOWN, -1 }
+};
+
+
